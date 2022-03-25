cf6a3215bf85900ee8618445bf6dde03e661608c
rusty-reebs <rusty_reebs@tutanota.com>
2022-03-25 14:23:25 -0600

    Navbar mobile styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,15 @@
 // Navbar.js
 
 //TODO no sandwich for mobile, just use hyperlinks across the top
+//TODO put current route in state and highlight nav link
 
 import React from "react";
 
 const NavBar = (props) => {
   return (
-    <div className="bg-zinc-700">
+    <div className="bg-zinc-700 opacity-90">
       <div
-        className="block font-merienda text-red-500 text-5xl pl-10"
+        className="block font-merienda text-red-500 text-5xl pl-10 leading-tight"
         display={{
           textShadowOffset: { width: "1.5px", height: "1.5px" },
           textShadowRadius: "3px",
@@ -17,11 +18,13 @@ const NavBar = (props) => {
       >
         🇬🇪 Nina's Georgian Restaurant 🇬🇪
       </div>
-      <nav className="font-quattro text-white">
+      <nav className="font-quattro text-white text-2xl">
         <ul className="flex flex-row justify-around">
-          <li>Home</li>
-          <li>Menu</li>
-          <li>Contact</li>
+          <li className=" hover:bg-black hover:text-white hover:rounded-lg">
+            Home
+          </li>
+          <li className=" hover:bg-black hover:text-white">Menu</li>
+          <li className=" hover:bg-black hover:text-white">Contact</li>
         </ul>
       </nav>
     </div>
