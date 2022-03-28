fce0808ba3a5868eb424d17e117ba6a79014983e
rusty-reebs <rusty_reebs@tutanota.com>
2022-03-28 15:04:14 -0600

    Set up Contact route

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,1 +1,24 @@
 // contact.js
+import React from "react";
+import tbilisi from "../img/tbilisi.jpeg";
+import kazbegi from "../img/kazbegi.jpeg";
+import NavBar from "./Navbar";
+
+const Contact = (props) => {
+  return (
+    <div
+      className="bg-top"
+      style={{
+        backgroundImage: `url(${kazbegi})`,
+        height: "100vh",
+        // opacity: "0.8",
+      }}
+    >
+      <NavBar />
+      <h2>Where To Find Us</h2>
+      <img src={tbilisi} alt="tbilsi" />
+    </div>
+  );
+};
+
+export default Contact;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "../src/output.css";
 import App from "./App";
 import Home from "./components/Home";
+import Contact from "./components/Contact";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,6 +12,8 @@ ReactDOM.render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/home" element={<Home />} />
+        {/* <Route path="/menu" element={<Menu />} /> */}
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
