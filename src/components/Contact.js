bef7db900ce65cc00500f11770345e4817afe192
rusty-reebs <rusty_reebs@tutanota.com>
2022-04-04 11:26:38 -0500

    Add styling and spacing

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -15,18 +15,18 @@ const Contact = (props) => {
       }}
     >
       <NavBar />
-      <div className="bg-zinc-700 opacity-90 px-3 py-5 font-quattro text-white">
+      <div className="bg-zinc-700 opacity-90 px-3 pt-5 pb-40 font-quattro text-white">
         <h2 className="text-xl text-center font-bold">Where To Find Us</h2>
-        <img src={tbilisi} alt="tbilsi" className="my-4 border-8 rounded-md" />
-        <div className="py-4">
-          <h2 className="text-xl text-center font-bold">
+        <img src={tbilisi} alt="tbilisi" className="my-4 border-8 rounded-md" />
+        <div className="mx-5 py-4">
+          <h2 className="text-xl text-center font-bold border-b-4 pb-6">
             We are located in the center of Tbilisi on Rustaveli Avenue.
           </h2>
-          <p className="h-4"></p>
+          <p className="h-8"></p>
           <h2 className="text-xl text-center font-bold">
             Find us on Facebook!
           </h2>
-          <p className="h-4"></p>
+          <p className="h-8"></p>
           <h2 className="text-xl text-center font-bold">
             Come and visit us today!
           </h2>
